25f7604f34d345708d220f1ca9cf119edda86f63
Peter Beshai <peter.beshai@gmail.com>
2016-10-15 16:26:15 -0400

    Make addComputedProps the default export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /**
  * The main library file. All top-level exports are listed here.
  */
+export default from './addComputedProps';
 export addComputedProps from './addComputedProps';
 export shallowEquals from './shallowEquals';
 export compose from './compose';
